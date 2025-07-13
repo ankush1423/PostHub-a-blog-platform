5240c65e6b08f04e29fd0d2d29ecf4dfcd470128
ankush1423 <139746905+ankush1423@users.noreply.github.com>
2025-07-13 14:47:41 +0530

    work on Home Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import {useEffect , useState} from 'react'
 import {Container,PostCard} from '../components'
 import appwriteServices from '../appwrite/config.js'
+import { Link } from 'react-router-dom'
 
 function Home() {
 
@@ -11,18 +12,21 @@ function Home() {
                             setPosts(posts.documents)
                         })
     },[])
- 
-    if(posts.length === 0)
+
+    if(posts?.length === 0 || posts === undefined)
     {
         return (
             <div className='w-full py-8 mt-4 text-center'>
                    <Container>
-                      <div className='flex flex-wrap'>
-                           <div className='p-2 w-full'>
+                      <div className='flex flex-wrap items-center justify-center
+                      '>
+                           <Link to="/login">
+                              <div className='p-2 w-full'>
                                 <h1 className='text-2xl font-bold hover:text-gray-500'>
                                      Login to read Posts
                                 </h1>
-                           </div>
+                             </div>
+                           </Link>
                       </div>
                    </Container>
             </div>
@@ -34,7 +38,7 @@ function Home() {
              <Container>
                  <div className='flex flex-wrap'>
                     {
-                      posts.map((post) => (
+                      posts?.map((post) => (
                           <div key={post.$id} className='p-2 w-1/5'>
                                <PostCard post={post}/>
                           </div>
